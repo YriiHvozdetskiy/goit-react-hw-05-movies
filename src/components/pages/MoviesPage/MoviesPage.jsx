aef1cc94c3d57f7ca07a4b43955880b57bc4b9e2
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-06 19:16:57 +0300

    додав щоб в url записувалися параметри

--- src/components/pages/MoviesPage/MoviesPage.jsx
+++ src/components/pages/MoviesPage/MoviesPage.jsx
@@ -1,21 +1,37 @@
 import {useEffect, useState} from 'react'
 import * as serverApi from '../../services/movies-api'
 import s from './MoviesPage.module.scss'
+import {useHistory, useLocation} from "react-router-dom";
 
 export default function MoviesPage() {
-	const [searchValue, setSearchValue] = useState(null	)
-	const [data,setData] = useState([])
+	const [searchValue, setSearchValue] = useState(null)
+	const [data, setData] = useState([])
 	const [error, setError] = useState('')
+	const location = useLocation()
+	const history = useHistory()
+
 
 	useEffect(() => {
-		if (!searchValue) return
+		const searchParam = new URLSearchParams(location.search).get('query')
+		if (searchParam !== null) {
+			async function fetchData() {
+				try {
+					const searchData = await serverApi.fetchMovieSearch(searchParam)
+					await setData(searchData)
+				} catch (error) {
+// приходе кастомне повідомлення error і записується в стейт
+					setError(error.message)
+				}
+			}
 
+			fetchData()
+		}
+		if (!searchValue) return
 
 		async function fetchData() {
 			try {
 				const searchData = await serverApi.fetchMovieSearch(searchValue)
 				await setData(searchData)
-
 			} catch (error) {
 // приходе кастомне повідомлення error і записується в стейт
 				setError(error.message)
@@ -23,20 +39,27 @@ export default function MoviesPage() {
 		}
 
 		fetchData()
+
+		history.push({
+			...location,
+			search: `query=${searchValue}`
+		})
 	}, [searchValue])
+
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		const {value} = e.target.value
-		setSearchValue(value)
+		// робем trim(удаляє пробіли в інпуті) щоб не робити fetch з пустою строкою і щоб не було пустої строки в location.search і за нею  не робився fetch
+		setSearchValue(value.trim())
 	}
 
 	return (<>
 		{error && <h1>{error}</h1>}
 		{/*TODO не рендирити форму при error */}
 		{data && <ul className={s.list}>
-			{data.map(({id,overview,budget,release_date,vote_average,title})=> {
-				return(
+			{data.map(({id, overview, budget, release_date, vote_average, title}) => {
+				return (
 					<li key={id} className={s.item}>
 						<h2>{title}</h2>
 						<p>{vote_average}</p>
@@ -44,7 +67,8 @@ export default function MoviesPage() {
 						<p>{budget}</p>
 						<p>{overview}</p>
 					</li>
-				)})}
+				)
+			})}
 		</ul>}
 		<form onSubmit={(e) => handleSubmit(e)}>
 			<input type="text" name='value'/>
