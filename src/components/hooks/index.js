bee8d14186650a711a159b01334bcf5f3714f2f7
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-07 13:58:07 +0300

    дод опис

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -17,7 +17,9 @@ export const useGoBack = () => {
 	const handleGoBack = () => {
 		//якщо зайшли з нової сторінки чи перезагрузили сторінку провіряєм чи є щось в routeState.current в противному випадку переходем на HomePage
 		const url = routeState.current ? routeState.current.from : '/'
+		// записуєм в історію куда потрібно перейти , а Route рендерить потрібну компоненту
 		history.push(`${url}`)
 	}
+		// повертаєм  обєкт з методом, щоб використовувати через кастомний хук в іншій компоненті
 	return {goBack: handleGoBack}
 }
