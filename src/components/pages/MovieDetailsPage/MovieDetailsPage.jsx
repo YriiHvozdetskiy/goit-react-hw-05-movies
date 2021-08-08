0d3ce8b9b95e1a82eb0b395a9b23aa25a740ea59
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-08 09:40:35 +0300

    рефактор MovieDetailsPage

--- src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,22 +1,24 @@
 import {useState, useEffect} from 'react';
-import {NavLink, Route, Switch,  useParams, useRouteMatch} from "react-router-dom";
+import {NavLink, Route, Switch, useParams, useRouteMatch} from "react-router-dom";
 import * as serverApi from '../../services/movies-api'
 import Cast from "../Cast/Cast";
 import Reviews from "../Reviews/Reviews";
 import {useGoBack} from '../../hooks/index'
 import s from './MovieDetailsPage.module.scss'
+import defaultImage from "../../../default.jpg";
 
 export default function MovieDetailsPage() {
 	const [movie, setMovie] = useState(null)
 	const [error, setError] = useState('')
 	const {movieId} = useParams()
 	const {url, path} = useRouteMatch()
-	const {goBack} =useGoBack()
+	const {goBack} = useGoBack()
 //TODO нормально стилізувати MovieDetailsPage
 	useEffect(() => {
 		async function fetchData() {
 			try {
 				const info = await serverApi.fetchMovieById(movieId)
+				console.log(info)
 				await setMovie(info)
 			} catch (error) {
 				// приходе кастомне повідомлення error і записується в стейт
@@ -26,21 +28,25 @@ export default function MovieDetailsPage() {
 
 		fetchData()
 	}, [movieId])
-
 	return (<div className={s.wrapper}>
 		<div className={s.info}>
 			{error && <h1>{error}</h1>}
 			{/*використовуєм кастомний хук щоб повернутися "назад"*/}
 			<button type="button" onClick={goBack}>Go back</button>
-			{movie && <h2>{movie.title}</h2>}
-			{movie && <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>}
-			{movie && <p>Vote {movie.vote_average}</p>}
-			{movie && <p>Release {movie.release_date}</p>}
-			{movie && <p>Budget {movie.budget} $</p>}
-			{movie && <p>{movie.overview}</p>}
-			{movie && <p>Genres {movie.genres.map(({name}) => {
-				return (<span>{name} </span>)
-			})}</p>}
+			{movie &&
+			(<ul>
+				<li key={movie.id}>
+					<h2>{movie.title}</h2>
+					{movie.poster_path && <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>}
+					{/*підставляєм дифолтну картинку якщо не приходе з бека  і ренд тільки один img де є картинка */}
+					{!movie.poster_path && <img src={defaultImage} alt={movie.title}/>}
+					<p>Vote {movie.vote_average}</p>
+					<p>Release {movie.release_date}</p>
+					<p>Budget {movie.budget} $</p>
+					<p>{movie.overview}</p>
+					<p>Genres {movie.genres.map(({name}) => (<span>{name} </span>))}</p>
+				</li>
+			</ul>)}
 		</div>
 		{/*навігація на сторінки які будуть рендиритися під цим ul( вложений  Route) */}
 		<ul className={s.box}>
@@ -68,3 +74,12 @@ export default function MovieDetailsPage() {
 }
 
 
+// {movie && <h2>{movie.title}</h2>}
+// {movie && <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>}
+// {movie && <p>Vote {movie.vote_average}</p>}
+// {movie && <p>Release {movie.release_date}</p>}
+// {movie && <p>Budget {movie.budget} $</p>}
+// {movie && <p>{movie.overview}</p>}
+// {movie && <p>Genres {movie.genres.map(({name}) => {
+// 	return (<span>{name} </span>)
+// })}</p>}
