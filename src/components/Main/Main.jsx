535a27db8eb7ea914c0d6f3ea03acb8b281504a8
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-08 11:52:38 +0300

    дод ліниву загрузку lazy

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,36 @@
+import {lazy,Suspense } from "react";
 import {Route, Switch} from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
-import MoviesPage from "../pages/MoviesPage/MoviesPage";
-import NotFoundView from "../pages/NotFoundView/NotFoundView";
-import MovieDetailsPage from "../pages/MovieDetailsPage/MovieDetailsPage";
+//лінива загрузка, грузиться коли переходем на цю компоненту
+// /* webpackChunkName: "MoviesPage" */  підписуєм компоненту при загрузці
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage' /* webpackChunkName: "MoviesPage" */))
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "MovieDetailsPage" */))
+const NotFoundView = lazy(() => import('../pages/NotFoundView/NotFoundView' /* webpackChunkName: "NotFoundView" */))
 
 export default function Main() {
-	return (<>
-		<main>
-			<section>
-				<Switch>
-					<Route exact path='/'>
-						<HomePage/>
-					</Route>
-					<Route path='/movies/' exact>
-						<MoviesPage/>
-					</Route>
-					<Route path='/movies/:movieId'>
-						<MovieDetailsPage/>
-					</Route>
-					<Route>
-						<NotFoundView/>
-					</Route>
-				</Switch>
-			</section>
-		</main>
-	</>)
+  return (<>
+	<main>
+	  <section>
+		{/*Suspense повинен обгортати тільки те що загружається при кліку*/}
+		{/* в fallback передаєм JSX який буде показуватися коли загр компонента */}
+		<Suspense  fallback={<h1>Loading...</h1>}>
+		  <Switch>
+			<Route exact path='/'>
+			  <HomePage/>
+			</Route>
+			<Route path='/movies/' exact>
+			  <MoviesPage/>
+			</Route>
+			<Route path='/movies/:movieId'>
+			  <MovieDetailsPage/>
+			</Route>
+			<Route>
+			  <NotFoundView/>
+			</Route>
+		  </Switch>
+		</Suspense >
+	  </section>
+	</main>
+  </>)
 }
 
--- src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,73 +1,78 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, Suspense, lazy} from 'react';
 import {NavLink, Route, Switch, useParams, useRouteMatch} from "react-router-dom";
 import * as serverApi from '../../services/movies-api'
-import Cast from "../Cast/Cast";
-import Reviews from "../Reviews/Reviews";
 import {useGoBack} from '../../hooks/index'
 import s from './MovieDetailsPage.module.scss'
 import defaultImage from "../../../default.jpg";
+//лінива загрузка, грузиться коли переходем на цю компоненту
+// /* webpackChunkName: "name" */  підписуєм компоненту при загрузці
+const Cast = lazy(() => import('../Cast/Cast' /* webpackChunkName: "Cast" */))
+const Reviews = lazy(() => import('../Reviews/Reviews' /* webpackChunkName: "Reviews" */))
 
 export default function MovieDetailsPage() {
-	const [movie, setMovie] = useState(null)
-	const [error, setError] = useState('')
-	const {movieId} = useParams()
-	const {url, path} = useRouteMatch()
-	const {goBack} = useGoBack()
+  const [movie, setMovie] = useState(null)
+  const [error, setError] = useState('')
+  const {movieId} = useParams()
+  const {url, path} = useRouteMatch()
+  const {goBack} = useGoBack()
 //TODO нормально стилізувати MovieDetailsPage
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				const info = await serverApi.fetchMovieById(movieId)
-				await setMovie(info)
-			} catch (error) {
-				// приходе кастомне повідомлення error і записується в стейт
-				setError(error.message)
-			}
-		}
+  useEffect(() => {
+	async function fetchData() {
+	  try {
+		const info = await serverApi.fetchMovieById(movieId)
+		await setMovie(info)
+	  } catch (error) {
+		// приходе кастомне повідомлення error і записується в стейт
+		setError(error.message)
+	  }
+	}
 
-		fetchData()
-	}, [movieId])
-	return (<div className={s.wrapper}>
-		<div className={s.info}>
-			{error && <h1>{error}</h1>}
-			{/*використовуєм кастомний хук щоб повернутися "назад"*/}
-			<button type="button" onClick={goBack}>Go back</button>
-			{movie &&
-			(<ul>
-				<li key={movie.id}>
-					<h2>{movie.title}</h2>
-					{movie.poster_path && <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>}
-					{/*підставляєм дифолтну картинку якщо не приходе з бека  і ренд тільки один img де є картинка */}
-					{!movie.poster_path && <img src={defaultImage} alt={movie.title}/>}
-					<p>Vote {movie.vote_average}</p>
-					<p>Release {movie.release_date}</p>
-					<p>Budget {movie.budget} $</p>
-					<p>{movie.overview}</p>
-					<p>Genres {movie.genres.map(({name}) => (<span>{name} </span>))}</p>
-				</li>
-			</ul>)}
-		</div>
-		{/*навігація на сторінки які будуть рендиритися під цим ul( вложений  Route) */}
-		<ul className={s.box}>
-			<li>
-				<NavLink to={`${url}/cast`} className={s.link} activeClassName={s.activeLink}>
-					Cast
-				</NavLink>
-			</li>
-			<li>
-				<NavLink to={`${url}/reviews`} className={s.link} activeClassName={s.activeLink}>
-					Reviews
-				</NavLink>
-			</li>
-		</ul>
+	fetchData()
+  }, [movieId])
+  return (<div className={s.wrapper}>
+	<div className={s.info}>
+	  {error && <h1>{error}</h1>}
+	  {/*використовуєм кастомний хук щоб повернутися "назад"*/}
+	  <button type="button" onClick={goBack}>Go back</button>
+	  {movie &&
+	  (<ul>
+		<li key={movie.id}>
+		  <h2>{movie.title}</h2>
+		  {movie.poster_path && <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>}
+		  {/*підставляєм дифолтну картинку якщо не приходе з бека  і ренд тільки один img де є картинка */}
+		  {!movie.poster_path && <img src={defaultImage} alt={movie.title}/>}
+		  <p>Vote {movie.vote_average}</p>
+		  <p>Release {movie.release_date}</p>
+		  <p>Budget {movie.budget} $</p>
+		  <p>{movie.overview}</p>
+		  <p>Genres {movie.genres.map(({name}) => (<span>{name} </span>))}</p>
+		</li>
+	  </ul>)}
+	</div>
+	{/*навігація на сторінки які будуть рендиритися під цим ul( вложений  Route) */}
+	<ul className={s.box}>
+	  <li>
+		<NavLink to={`${url}/cast`} className={s.link} activeClassName={s.activeLink}>
+		  Cast
+		</NavLink>
+	  </li>
+	  <li>
+		<NavLink to={`${url}/reviews`} className={s.link} activeClassName={s.activeLink}>
+		  Reviews
+		</NavLink>
+	  </li>
+	</ul>
+	{/*Suspense повинен обгортати тільки те що загружається при кліку -- не все в компоненті */}
+	<Suspense>
+	  <Switch>
 		{/*коли шлях в Route path буде співпадати з NavLink to: to={`${url}/cast`}  ===  path={`${path}/cast`}, то буде рендитися компонента між Route під ul  */}
-		<Switch>
-			<Route path={`${path}/cast`}>
-				<Cast/>
-			</Route>
-			<Route path={`${path}/reviews`}>
-				<Reviews/>
-			</Route>
-		</Switch>
-	</div>)
+		<Route path={`${path}/cast`}>
+		  <Cast/>
+		</Route>
+		<Route path={`${path}/reviews`}>
+		  <Reviews/>
+		</Route>
+	  </Switch>
+	</Suspense>
+  </div>)
 }
